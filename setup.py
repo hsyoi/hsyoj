b27bf0082991ecc78b540bba8eb10cbfab43761e
hr567 <hr567q@163.com>
2018-05-23 21:53:42 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='hsyoj',
+    version='0.0.1',
+    packages=[],
+    url='https://gitlab.com/hsyoj/hsyoj',
+    license='AGPLv3',
+    author='hr567',
+    author_email='hr567q@163.com',
+    requires=['Django'],
+    description='Online Judge for NO.1 Middle School Affiliated to CCNU',
+)

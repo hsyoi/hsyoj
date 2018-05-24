d89afc27a95121072742626c561c667269943d8c
hr567 <hr567q@163.com>
2018-05-25 00:07:28 +0800

    Add problems' route

--- hsyoj/urls.py
+++ hsyoj/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from problems import views
+
+from . import views
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
+    path('problems/', include('problems.urls')),
 ]
--- hsyoj/views.py
+++ hsyoj/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Thers is nothing now.")
+    return HttpResponse("Welcome to HSYOJ.")

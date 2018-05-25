b9e5b2c772a54a747e4f67f9fecb9d3a1ee2a7bb
hr567 <hr567q@163.com>
2018-05-25 14:23:11 +0800

    Add records

--- hsyoj/urls.py
+++ hsyoj/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('problems/', include('problems.urls')),
     path('users/', include('users.urls')),
+    path('records/', include('records.urls')),
 ]

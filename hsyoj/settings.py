84825bb6b3c66b918ceae93180dc3e6dba7f0e6a
hr567 <hr567q@163.com>
2018-05-25 14:24:04 +0800

    Install users and records apps

--- hsyoj/settings.py
+++ hsyoj/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'problems.apps.ProblemsConfig',
+    'users.apps.UsersConfig',
+    'records.apps.RecordsConfig',
 ]
 
 MIDDLEWARE = [

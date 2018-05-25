c31a289762cc9a3204c03e2e293b59edf3146c4e
hr567 <hr567q@163.com>
2018-05-25 14:22:54 +0800

    Add users

--- hsyoj/urls.py
+++ hsyoj/urls.py
@@ -16,12 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from problems import views
-
 from . import views
 
 urlpatterns = [
     path('', views.index),
     path('admin/', admin.site.urls),
     path('problems/', include('problems.urls')),
+    path('users/', include('users.urls')),
 ]

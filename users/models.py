697b4b0286b9d8fe6756acbb0107d90dd88d4bb7
hr567 <hr567q@163.com>
2018-05-25 19:09:10 +0800

    Add User's fields

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    user_id = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=64)
+    password = models.CharField(max_length=512)
+    phone_number = models.DecimalField(max_digits=11, decimal_places=11)
+    email = models.EmailField()
+    register_time = models.DateTimeField()

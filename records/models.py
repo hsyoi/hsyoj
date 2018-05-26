417b710da16764a478e2c8bcf9f4a9e3500a1bb4
hr567 <hr567q@163.com>
2018-05-26 20:16:27 +0800

    Make some fields uneditable

--- records/models.py
+++ records/models.py
@@ -5,6 +5,6 @@ class Record(models.Model):
     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
     problem = models.ForeignKey(
         "problems.Problem", on_delete=models.SET_NULL, null=True)
-    code = models.TextField()
-    submit_time = models.DateTimeField(auto_now_add=True)
-    running_time = models.DurationField()
+    code = models.TextField(editable=False)
+    submit_time = models.DateTimeField(auto_now_add=True, editable=False)
+    running_time = models.DurationField(editable=False)
--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
     password = models.CharField(max_length=512)
     phone_number = models.DecimalField(max_digits=11, decimal_places=11)
     email = models.EmailField()
-    register_time = models.DateTimeField(auto_now_add=True)
+    register_time = models.DateTimeField(auto_now_add=True, editable=False)
 
     def __str__(self):
         return f"{self.id}: {self.username}"

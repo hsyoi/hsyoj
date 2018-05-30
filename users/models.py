83a38db1a89a8c45a1b515a9a7982891a69cceee
hr567 <hr567q@163.com>
2018-05-30 17:43:36 +0800

    Fix phone number setting error

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=64)
     password = models.CharField(max_length=512)
-    phone_number = models.DecimalField(max_digits=11, decimal_places=11)
+    phone_number = models.DecimalField(max_digits=11, decimal_places=0)
     email = models.EmailField()
     register_time = models.DateTimeField(auto_now_add=True, editable=False)
 
